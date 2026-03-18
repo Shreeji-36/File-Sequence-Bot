a9745842ad2c2320ee9361a9639e63c4189067dc
Shreeji-36 <trivediss158@gmail.com>
2026-03-18 14:35:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class Bot(Client):
             name="Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={"root": "Plugins"},
+            plugins={"root": "plugins"},
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN,
         )
